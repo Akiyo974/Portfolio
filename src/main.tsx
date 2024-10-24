8da1b4ab67ea4e5ea6428d2bb80eb79c2a0ff5c7
Christen dijoux <christen.dijoux@gmail.com>
2024-10-24 01:59:14 -0400

    Save current changes before rebase

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
 
@@ -10,9 +10,9 @@ const basename = import.meta.env.MODE === 'production' ? '/portfolio' : '/';
 if (root) {
   createRoot(root).render(
     <StrictMode>
-      <BrowserRouter basename={basename}>
+      <HashRouter basename={basename}>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </StrictMode>
   );
 }
